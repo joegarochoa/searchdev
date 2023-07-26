7996777b2ef1d72c543126fad08a7a48028466c5
joegarochoa <joegarochoa@yahoo.com>
2023-07-26 14:04:15 +0800

    hostname

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -16,6 +16,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+CUSTOM_BASE_DIR = BASE_DIR
 
 
 # Quick-start development settings - unsuitable for production
--- static/js/main.js
+++ static/js/main.js
@@ -26,9 +26,10 @@ for (let i=0; tags.length > i; i++){
     tags[i].addEventListener('click', (e) => {
         let tagId = e.target.dataset.tag 
         let projectId = e.target.dataset.project
+        let hostname = e.target.dataset.host
 
         //console.log('Tag ID: ', tagId, 'Project ID: ', projectId)
-        fetch(process.env.HOST+'/api/remove-tag/', {
+        fetch(hostname+'/api/remove-tag/', {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json'
